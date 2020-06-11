5fa7ddbd5052bb54fc8be8a39a06b742a7d7e309
yazzy4 <yazburrell@pursuit.org>
2020-06-11 15:02:00 -0400

    first commit

---
+++ FoodPicker/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FoodPicker
+//
+//  Created by Yaz Burrell on 6/11/20.
+//  Copyright © 2020 Yaz Burrell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let hello = "Sup"
+        let world = "World"
+        print("\(hello)" + " " + "\(world)")
+    
+    }
+
+
+}
+
