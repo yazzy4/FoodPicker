0cb6eb9753fea1b5f97e3c487f6bb5d969e2a962
yazzy4 <yazburrell@pursuit.org>
2020-06-11 17:34:20 -0400

    Random food types appear when choose food type button is pressed

--- FoodPicker/ViewController.swift
+++ FoodPicker/ViewController.swift
@@ -10,14 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let hello = "Sup"
-        let world = "World"
-        print("\(hello)" + " " + "\(world)")
+    @IBOutlet weak var todaysChoiceLabel: UILabel!
+    @IBOutlet weak var foodTypeLabel: UILabel!
+    @IBOutlet weak var chooseButton: UIButton!
     
+    let foodType: [String] = ["American", "Thai", "Chinese", "Italian", "Soul Food", "Comfort Food", "Indian", "Mexican", "Ethiopian", "Japanese", "French", "Creole", "West African", "Fast Food", "Tex Mex", "Vegan", "Seafood", "BBQ", "Vegan BBQ", "Diner Food", "Greek"]
+    
+    
+    @IBAction func didSelectChooseFoodButton(_ sender: UIButton) {
+        todaysChoiceLabel.isHidden = false
+        foodTypeLabel.isHidden = false
+        
+        chooseButton.setTitle("Choose Again", for: .normal)
+        
+        let foodTypeCount = UInt32(foodType.count)
+        let randomNumber = arc4random_uniform(foodTypeCount)
+        
+        let intRandomNumber = Int(randomNumber)
+        let food = foodType[intRandomNumber]
+        
+        foodTypeLabel.text = food
+        
     }
+    
 
 
 }
